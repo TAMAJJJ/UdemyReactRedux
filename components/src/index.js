cd6d8f59b7d6bff1fbf69128bfe1b15234c882ee
TAMA <tamajessica18@gmail.com>
2020-05-02 20:45:51 -0400

    beginning of seasons

---
+++ components/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+    return (<div className="ui container comments">
+        <CommentDetail author="Sam"/>
+        <CommentDetail author="Alex"/>
+        <CommentDetail author="Jessie"/>
+    </div>);
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
